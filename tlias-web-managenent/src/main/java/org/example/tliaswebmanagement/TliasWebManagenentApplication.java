c0d5cae43d89c4390dd24c62acb3f71d80396f90
恒 <2319887702@qq.com>
2025-08-27 22:12:37 +0800

    标记自己的信息

--- tlias-web-managenent/src/main/java/org/example/tliaswebmanagement/TliasWebManagenentApplication.java
+++ tlias-web-managenent/src/main/java/org/example/tliaswebmanagement/TliasWebManagenentApplication.java
@@ -3,7 +3,7 @@ package org.example.tliaswebmanagement;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//2319887702@qq.com
 @SpringBootApplication
 public class TliasWebManagenentApplication {
 
